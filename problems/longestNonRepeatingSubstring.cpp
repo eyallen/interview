3ce2282e61cb76be79e862c254da9a3abc8618a0
Alexander Allen <alexander.ryan.allen@gmail.com>
2017-10-24 22:16:00 -0700

    longest substring

---
+++ problems/longestNonRepeatingSubstring.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+// abcabcbb
+// ji
+int lengthOfLongestSubstring(string s) 
+{
+    if (s.length() == 0) return 0;
+
+    unordered_map<char, int> charMap;
+    int maxLength = 0;
+    for (int i=0,j=0; i < s.length(); ++i)
+    {
+        auto c = s[i];
+        if (charMap.count(c))
+        {
+            j = max(j, charMap[c] + 1);
+        }
+
+        charMap[c] = i;
+        maxLength = max(maxLength, i-j+1);
+    }
+
+    return maxLength;
+}
+
+int main()
+{
+    cout << lengthOfLongestSubstring("abcabcbb") << "\n";
+}
