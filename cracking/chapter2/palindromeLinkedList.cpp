516dc6b0dd0c05c11237084141da66e688a3221e
Alexander Allen <alexander.ryan.allen@gmail.com>
2017-11-08 19:37:47 -0800

    mutliple palindrome options

--- cracking/chapter2/palindromeLinkedList.cpp
+++ cracking/chapter2/palindromeLinkedList.cpp
@@ -4,6 +4,45 @@
 
 using namespace std;
 
+// 1->2->1
+
+// When we don't know the length
+bool isPalindrome(Node* head)
+{
+    stack<int> valStack;
+    Node* slow = head;
+    Node* fast = head;
+
+    while (slow != nullptr && fast->next != nullptr)
+    {
+        valStack.push(slow->value);
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    if (fast != nullptr)
+    {
+        // Ignore the middle element in odd sized lists
+        slow = slow->next;
+    }
+
+    while(slow != nullptr)
+    {
+        int val = valStack.top();
+        valStack.pop();
+
+        if (val != slow->value)
+        {
+            return false;
+        }
+
+        slow = slow->next;
+    }
+
+    return true;
+}
+
+// When we know the length
 bool isPalindrome(Node* head, int size)
 {
     if (head == nullptr || size == 0)
@@ -49,6 +88,13 @@ int main()
 {
     vector<int> vec = {1,0,1,0,1};
     Node* list = vectorToLinkedList(vec);
-    bool p = isPalindrome(list, vec.size());
-    cout << p;
+    
+    cout << isPalindrome(list, 5) << endl;
+    cout << isPalindrome(list) << endl;
+
+    vec = {1,2,1,0,1};
+    list = vectorToLinkedList(vec);
+    
+    cout << isPalindrome(list, 5) << endl;
+    cout << isPalindrome(list) << endl;
 }
