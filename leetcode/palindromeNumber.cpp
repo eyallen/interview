3c99141dc9da40defa5e09ba4bf962390c452b65
Alexander Allen <alexander.ryan.allen@gmail.com>
2017-11-27 21:27:48 -0800

    palindrom number

--- leetcode/palindromeNumber.cpp
+++ leetcode/palindromeNumber.cpp
@@ -7,8 +7,25 @@ using namespace std;
  */
 bool isPalindrome(int x)
 {
+    if (x < 0)
+    {
+        return false;
+    }
+
+    int digit = 0;
+    int reversed = 0;
+    int temp = x;
+    while (temp > 0)
+    {
+        reversed *= 10;
+        reversed += temp % 10;
+        temp /= 10;
+    }
+
+    return reversed == x;
 }
 
 int main()
 {
+    cout << isPalindrome(1002) << endl;
 }
