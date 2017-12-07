eb13d3dd56a9966f5a922a764e5a719d232b4899
Alexander Allen <alexander.ryan.allen@gmail.com>
2017-12-06 21:10:24 -0800

    robots

--- cracking/chapter8/robotInGrid.cpp
+++ cracking/chapter8/robotInGrid.cpp
@@ -19,36 +19,28 @@ struct Tile
  * Certains cells are off limits such that the robot cannot move there.
  * Design an algorithm to determine a path to the bottom right
  */
-vector<Tile> getValidPath(Tile map[2][2], int r, int c)
+bool getValidPath(Tile map[2][2], int r, int c, vector<Tile>& path)
 {
     // Assume we don't get passed a value > map size
+    // TODO: We should store off tiles we already visition for O(rc);
     if (r < 0 || c < 0 || !map[r][c].isValid)
     {
-        return {};
+        return false;
     }
 
     if (r == 0 && c == 0)
     {
-        vector<Tile> path = vector<Tile>();
         path.push_back(map[0][0]);
-        return path;
+        return true;
     }
 
-    vector<Tile> pathUp = getValidPath(map, r-1, c);
-    vector<Tile> pathLeft = getValidPath(map, r, c-1);
-
-    if (!pathUp.empty())
+    if (getValidPath(map, r-1, c, path) || getValidPath(map, r, c-1, path))
     {
-        pathUp.push_back(map[r][c]);
-        return pathUp;
+        path.push_back(map[r][c]);
+        return true;
     }
-    if (!pathLeft.empty())
-    {
-        pathLeft.push_back(map[r][c]);
-        return pathLeft;
-    }
-
-    return {};
+    
+    return false;
 }
 
 int main()
@@ -63,7 +55,8 @@ int main()
         { Tile(1,0,true), Tile(1,1,true)},
     };
 
-    vector<Tile> path = getValidPath(map, 1, 1);
+    vector<Tile> path = vector<Tile>();
+    cout <<  getValidPath(map, 1, 1, path);
 
     /*
      *   | |X|
@@ -75,7 +68,8 @@ int main()
         { Tile(1,0,true), Tile(1,1,true)},
     };
 
-    vector<Tile> path2 = getValidPath(map2, 1, 1);
+    vector<Tile> path2 = vector<Tile>();
+    cout << getValidPath(map2, 1, 1, path2);
 
     /*
      *   | | |
@@ -87,5 +81,6 @@ int main()
         { Tile(1,0,true), Tile(1,1,false)},
     };
 
-    vector<Tile> path3 = getValidPath(map3, 1, 1);
+    vector<Tile> path3 = vector<Tile>();
+    cout << getValidPath(map3, 1, 1, path3);
 }
