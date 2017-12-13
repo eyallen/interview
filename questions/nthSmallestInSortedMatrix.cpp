0c21da2bb7d8e79fc075267c6bc7452713d0676e
Alexander Allen <alexander.ryan.allen@gmail.com>
2017-12-13 11:33:52 -0800

    nth smallest in matrix

---
+++ questions/nthSmallestInSortedMatrix.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int findNthSmallest(vector<vector<int>> matrix, int n)
+{
+    priority_queue<int> q = priority_queue<int>();
+
+    for (int r = 0; r < matrix.size(); ++r)
+    {
+        for (int c = 0; c < matrix[0].size(); ++c)
+        {
+            if (q.empty() || q.size() <= n)
+            {
+                q.push(matrix[r][c]);
+            }
+            else if (matrix[r][c] < q.top())
+            {
+                q.pop();
+                q.push(matrix[r][c]);
+            }
+        }
+    }
+
+    return q.top();
+}
+
+int main()
+{
+    vector<vector<int>> m = 
+    {
+        {0, 1, 2},
+        {3, 4, 7}
+    };
+
+    cout << findNthSmallest(m, 5) << endl;
+}
